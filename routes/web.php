e7fc25305789eeadb1c1ae1e4c2d1983eb1d5d37
Ardhiyas <ardhiyasss8@gmail.com>
2025-11-26 11:51:07 +0700

    nwroute

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-    return view('pages.dashboard');
-});
 
 Route::get('/', [PagesController::class, 'index'])->name('home');
