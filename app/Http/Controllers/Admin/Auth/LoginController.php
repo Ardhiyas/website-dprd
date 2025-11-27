80bc54004a9c86f5c285c670fe9d3c6374be8c88
Ardhiyas <ardhiyasss8@gmail.com>
2025-11-27 13:17:47 +0700

    BE-Admin

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (auth()->guard('web')->attempt($credentials)) {
+            return redirect()->intended(route('admin.dashboard'));
+        } else {
+            return redirect()->back()->withInput($request->only('email'));
+        }
+    }
+
+    public function logout()
+    {
+        auth()->guard('web')->logout();
+        return redirect()->route('admin.login');
+    }
+}
